2949785a1101349930331bd2e86adf9cfb5c5004
s87thafe <s87thafe@uni-bonn.de>
2024-02-29 17:57:56 +0100

    'Initial commit.'

---
+++ src/arbitrage_analysis/__init__.py
@@ -0,0 +1,1 @@
+"""Code for producing the results of the Arbitrage Analysis in the Sports Betting Market project."""
---
+++ src/arbitrage_analysis/final/__init__.py
@@ -0,0 +1,5 @@
+"""Functions for formatting results."""
+
+from arbitrage_analysis.final.plot import plot_regression_by_age
+
+__all__ = [plot_regression_by_age]
