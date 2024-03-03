abfb89d535988cda347d9601b5f56be4831789f2
s87thafe <s87thafe@uni-bonn.de>
2024-03-03 11:42:33 +0100

    Implement Pytask to denest df_odds.csv

---
+++ src/arbitrage_analysis/data_management/task_retrieve_best_odds.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import json
+from arbitrage_analysis.config import BLD_data, SRC
+
+
+def create_odds_dataframe(bookmakers_json, home_team, away_team):
+    """
+    Creates a DataFrame containing the odds offered by bookmakers for a given game between a home team and an away team.
+    
+    Parameters:
+    - bookmakers_json (str): A JSON string containing the bookmakers' odds data.
+    - home_team (str): The name of the home team.
+    - away_team (str): The name of the away team.
+    
+    The function parses the JSON data to extract the odds for the home win, draw, and away win offered by each bookmaker.
+    
+    Returns:
+    - pandas.DataFrame: A DataFrame where each row represents the odds from a different bookmaker, including columns for
+      the bookmaker's name, the home team, the away team, odds for home win, draw, and away win.
+    """
+    bookmakers_data = json.loads(bookmakers_json.replace("'", "\""))
+    bookmakers_odds = []
+
+    for bookmaker in bookmakers_data:
+        bookmaker_odds = {
+            'bookmaker': bookmaker['title'],
+            'home_team': home_team,  # Add home team name
+            'away_team': away_team,  # Add away team name
+            'home_win_odds': None,
+            'draw_odds': None,
+            'away_win_odds': None
+        }
+        for market in bookmaker['markets']:
+            if market['key'] == 'h2h':
+                for outcome in market['outcomes']:
+                    if outcome['name'] == home_team:
+                        bookmaker_odds['home_win_odds'] = outcome['price']
+                    elif outcome['name'] == away_team:
+                        bookmaker_odds['away_win_odds'] = outcome['price']
+                    elif outcome['name'] == 'Draw':
+                        bookmaker_odds['draw_odds'] = outcome['price']
+        bookmakers_odds.append(bookmaker_odds)
+
+    return pd.DataFrame(bookmakers_odds)
+
+create_odds_dataframe_depends_on = {
+    "data": SRC / "data" / "df_odds.csv",
+    "directory": BLD_data / ".dir_created"
+}
+
+def task_create_odds_dataframe(
+        depends_on= create_odds_dataframe_depends_on,
+        produces=BLD_data / "df_all_games_odds.csv",
+):
+    # Load the dataset containing the odds data
+    df_odds = pd.read_csv(depends_on["data"])
+    # Initialize an empty list to store each game's DataFrame
+    all_games_odds = []
+    # Iterate over each game in the dataset
+    for index, row in df_odds.iterrows():
+        game_odds_df = create_odds_dataframe(row['bookmakers'], row['home_team'], row['away_team'])
+        all_games_odds.append(game_odds_df)
+
+    # Concatenate all individual DataFrames into a single DataFrame
+    df_all_games_odds = pd.concat(all_games_odds, ignore_index=True)
+    # Display the combined DataFrame
+    df_all_games_odds.to_csv(produces, index=False)
--- src/arbitrage_analysis/task_folder_structure.py
+++ src/arbitrage_analysis/task_folder_structure.py
@@ -1,16 +1,30 @@
 from arbitrage_analysis.config import BLD
 
 # Define the subdirectories to create within BLD
-subdirectories = {
+files_produces = {
+    "data": BLD / "data" / ".dir_created",
+    "figures": BLD / "figures" / ".dir_created",
+    "models": BLD / "models" / ".dir_created",
+    "predictions": BLD / "predictions" / ".dir_created",
+    "tables": BLD / "tables" / ".dir_created",
+}
+
+def task_create_directories(
+    produces=files_produces,
+):
+    # Define the subdirectories to create within BLD
+    directories = {
     "data": BLD / "data",
     "figures": BLD / "figures",
     "models": BLD / "models",
     "predictions": BLD / "predictions",
-    "tables": BLD / "tables"
-}
-
-def task_create_directories(
-    produces=subdirectories.values(),
-):
-    for directory in subdirectories.values():
+    "tables": BLD / "tables",
+    }
+    for directory in directories.values():
+        # Create the subdirectory if it doesn't exist
         directory.mkdir(parents=True, exist_ok=True)
+
+    for file in produces.values():
+        # Create a marker file in each subdirectory
+        marker_file = file
+        marker_file.touch()
